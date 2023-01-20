7d2a6c78c8a90a7611029da882cfa86dc7e8eb4d
Lukas Dönges <lukasdoenges@t-online.de>
2023-01-20 23:06:12 +0100

    added testEnvBuilder

---
+++ envBuilder/startup.sh
@@ -0,0 +1,14 @@
+
+cat > ./env.ts <<EOF
+import { env } from './interface/envinterface';
+//overrided by startup.sh
+export const envVariables: env = {
+    sparkyurl: 'http://sparky-service:$SPARKY_PORT',
+    backendurl: 'http://stu-mgmt-backend:$BACKEND_PORT',
+    submissionurl: 'http://exercise-submitter-server:$SUBMISSION_SERVER_PORT',
+    courseid: '$SUBMISSION_WEB_CLIENT_COURSEID'
+};
+EOF
+
+echo "Running $@"
+exec "$@"
